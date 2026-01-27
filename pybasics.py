969e4feedd66a23907e482990f81b9d040cbb58d
dharanik531-arch <dharanik531@gmail.com>
2026-01-27 15:30:42 +0530

    Add files via upload

---
+++ pybasics.py
@@ -0,0 +1,84 @@
+#modulas
+'''a=10
+b=15
+
+c=a%b
+print(c)'''
+
+#formula
+
+'''a=10
+
+formula= a**0.5 
+
+print ("formula",formula)'''
+
+#area of triangle
+
+'''base=2.6
+height=5
+
+formula=0.5*base*height
+print ("formula ",formula)'''
+
+#Exponent
+'''a=5
+b=2
+
+c=(a+b)**2
+c=(a-b)**2
+c=a**2-b**2
+print (c)'''
+
+# swap temp
+
+'''a=10
+b=20
+
+temp=a
+a=b
+b=temp
+
+print ("after the swap A",a)
+print ("after the swap B",b)'''
+
+#pro kilo m to miles
+
+'''kilometer=2
+
+kilo =a*0.63
+
+print ("kilo",kilo)'''
+
+#celcious
+'''
+a=5.6
+formula=(5.6*1.8)+32
+print ("formula",formula)'''
+
+#last digit l
+'''a=101
+
+formula=a%10
+
+print ("formula",formula)'''
+
+#last two digit
+'''a=675
+
+formula =a%100
+
+print ("formula",formula)'''
+
+#take five digit number as ip and square the middle vale and print the sqare
+
+'''num=56789
+
+middle_num=(56789//100)%10
+square_num=middle_num**2
+print ("middle_num",middle_num)
+print  ("square_num",square_num)'''
+
+
+
+
---
+++ pyloop.py
@@ -0,0 +1,7 @@
+#for loop and while loop
+# find the first n natural numbers
+n=[1,2,3,5,8,9,7,4,9]
+
+for i in range (1,n+1,1):
+    if (n[i] in i ):
+   print(i)
