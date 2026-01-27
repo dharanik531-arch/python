cf7c3e6b1b042ac847e3ee641905eb9b0fe75941
dharanik531-arch <dharanik531@gmail.com>
2026-01-27 15:21:43 +0530

    Add files via upload

---
+++ python backend .py
@@ -0,0 +1,306 @@
+#modulas
+'''a=10
+b=15
+
+c=a%b
+print(c)'''
+
+#formula
+
+'''a=10
+
+formula= a**0.5 
+
+print ("formula",formula)'''
+
+#area of triangle
+
+'''base=2.6
+height=5
+
+formula=0.5*base*height
+print ("formula ",formula)'''
+
+#Exponent
+'''a=5
+b=2
+
+c=(a+b)**2
+c=(a-b)**2
+c=a**2-b**2
+print (c)'''
+
+# swap temp
+
+'''a=10
+b=20
+
+temp=a
+a=b
+b=temp
+
+print ("after the swap A",a)
+print ("after the swap B",b)'''
+
+#pro kilo m to miles
+
+'''kilometer=2
+
+kilo =a*0.63
+
+print ("kilo",kilo)'''
+
+#celcious
+'''
+a=5.6
+formula=(5.6*1.8)+32
+print ("formula",formula)'''
+
+#last digit l
+'''a=101
+
+formula=a%10
+
+print ("formula",formula)'''
+
+#last two digit
+'''a=675
+
+formula =a%100
+
+print ("formula",formula)'''
+
+#take five digit number as ip and square the middle vale and print the sqare
+
+'''num=56789
+
+middle_num=(56789//100)%10
+square_num=middle_num**2
+print ("middle_num",middle_num)
+print  ("square_num",square_num)'''
+
+'''>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'''
+
+
+#task 2
+
+# 1
+'''a=30
+b=50
+
+if (a<b) or (b<a) :
+   print ("the value a is smaller", a)
+
+else :
+    print ( "the value b is greater" ,b)
+    '''
+#2
+'''a=4564
+b=35452
+
+if (a>b):
+     print ("the value a is largest",a)
+
+else :
+     print (" the value b is largest",b)'''
+
+#3
+'''number = -25
+if number<0:
+    absolute_value=-number
+    
+else :
+     absolute_value=number
+
+print("absolute_value",number)'''
+
+#4
+'''a=int(input("enter the value a"))
+
+
+if (a%2==0) :
+    print (" the given value is even")
+else :
+    print ("the given number is odd") '''
+#5
+
+'''a=int(input("enter the value"))
+
+if (a%5==0):
+    print (" the value is multiple by 5")
+else :
+     print (" the value is  not satisfied")'''
+#6
+'''a=int(input("enter the value ="))
+
+if (a%10==0):
+    print (" the value is multiple of 100")
+
+else :
+     print (" the value is not satisfied")'''
+#7
+'''a=int(input("enter the value = "))
+if (a>9 and a<=99):
+ print ( " the value is two digit number :",a)
+
+else :
+    print ("the value is single digit :",a)'''
+#8
+'''a=int(input("enter the value ="))
+
+if(a>99 and a<1000 ) :
+      print (" the given value is three  digit :", a)
+else :
+    print ( " the given value is less then three digit :",a)'''
+#9
+'''a=int(input("enter the value ="))
+
+if (a%10==0):
+    print (" the value is end with zero :",a)
+else :
+    print (" the value is not end with zero:",a)'''
+#10
+''''a=int(input(" enter the value ="))
+a=a**2
+if (a>50):
+    print (" the value is square above 50 :",a)
+
+else:
+   print ("the value not square of 50 :",a)'''
+#11
+'''a=int(input("enter the value :"))
+b=int(input("enter the value :"))
+
+num =a-b
+if (num%10==0):
+    print (" the value is end with zero :",num)
+else :
+    print ("the value is not end with zero :",num)'''
+#12
+
+'''b=int(input("enter the mark :"))
+
+if (b>=50):
+    print ( " the student has be pass :",b)
+
+else :
+    print ("the student is failed :",b)'''
+#13
+'''a=int(input("enter the nuber ="))
+
+if (a%10==0):
+    print (" the value is divisible by 10 :",a)
+else :
+    print (" the value is not divisible")'''
+
+#14
+
+'''a=int(input("enter the value for two digit :"))
+
+print (a)
+
+b=a%10
+print (b)
+c=a//10
+
+if b>c :
+    print ("the largest two digit number",b)
+
+else :
+    print ("the largest c value ",c)'''
+
+#15
+'''choice=int(input("enter the value to acccept :"))
+
+if choice==1 :
+    print (" the exam will be easy :",choice)
+
+else :
+    print ("the exam will difficult :",choice)'''
+
+#16
+
+'''play=int(input("enter the value to play :"))
+
+if play==1 :
+    print ("you can go out and play")
+
+else :
+    print ("you not go out")'''
+
+#17
+
+'''length = int(input("enter the value "))
+breadth =int(input("enter the value"))
+
+if length==breadth :
+    print (" the value is square : ")
+
+else :
+    print ("the shape is rectangle ")'''
+#18
+'''a=int(input("enter the value 65 :"))
+
+if (a>=65 and a<=90):
+    print ("the value is upper case ASCII A ")
+
+else :
+    print ("the value is not ASCII A")'''
+
+#19
+
+'''a=int(input("enter the value 97:"))
+
+if (a>=97 and a<=122):
+    print ("the value is lower case ASCII")
+
+else :
+    print ("the value is not lower case")'''
+
+
+#20
+
+'''a=int(input("enter the value of numeric 0 :"))
+
+if (a>=48 and a<=97):
+    print ("the value is numeric ASCII :",a)
+
+else :
+    print ("the value is not ASCII :",a)'''
+
+#21 
+
+'''for j in range(1,6,1):
+    for i in range(1,5,1):
+      print("*",end="")
+    print()'''
+
+
+'''>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+------------------------------------------------------------------------------------------------------'''
+
+#palindrome
+
+'''n=input("enter the number :")
+rev=n[::-1]
+
+if rev==n :
+    print ("palindrome",n)
+
+else :
+    print ("not palindrome",n)'''
+
+#palindrome without using function :
+
+n=str(input("enter the string"))
+rev=""
+
+n=len(n)-1
+if
+    
+
+
+
+
+
+
